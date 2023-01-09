7663138cecbbebf7e2fddb92d0a03b4389815fb2
Surajk37 <surajkhot9993@gmail.com>
2023-01-09 18:29:15 +0530

    Ability to create a Stack of 56->30->70

---
+++ StackAndQueues/LinkedListStack.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueues
+{
+    class LinkedListStack
+    {
+        public Node top;
+
+        //Constructor
+        public LinkedListStack()
+        {
+            this.top = null;
+        }
+
+        //Push Data to stack
+        public void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+
+            }
+            this.top = node;
+
+            //Display Added Data value
+            Console.WriteLine("{0} : pushed  to Stack  ", value);
+        }
+
+        //Display Stack
+        public void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.WriteLine("Stack data : " + temp.data);
+                temp = temp.next;
+            }
+        }
+    }
+}
--- StackAndQueues/Program.cs
+++ StackAndQueues/Program.cs
@@ -5,6 +5,15 @@
         static void Main(string[] args)
         {
             Console.WriteLine("\t\tWelcome To Stack And Queues Problems\n");
+            //Calling Class and Adding Data in Stack
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.Push(70);
+            linkedListStack.Push(30);
+            linkedListStack.Push(56);
+
+            //Display Message
+            Console.WriteLine("\n");
+            linkedListStack.Display();
             Console.ReadLine();
         }
     }
