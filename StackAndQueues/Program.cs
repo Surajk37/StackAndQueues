cf06d9f44a9682129aea0576239c62b4a3367c78
Surajk37 <surajkhot9993@gmail.com>
2023-01-10 15:59:57 +0530

    Created a Queue of 56->30->70

--- StackAndQueues/Program.cs
+++ StackAndQueues/Program.cs
@@ -6,14 +6,17 @@
         {
             Console.WriteLine("\t\tWelcome To Stack And Queues Problems\n");
             Console.WriteLine("Please Select Option From List");
-            Console.WriteLine("\n1. Create a Stack \n2. To Peek And Pop the Element from stack");
+            Console.WriteLine("\n1. Create a Stack \n2. To Peek And Pop the Element from stack \n3. Create a Queue");
             int option = int.Parse(Console.ReadLine());
-            //Calling Class 
+            //Calling Class and addind data to stack
             LinkedListStack linkedListStack = new LinkedListStack();
+            
+            //Calling Class and Adding Data in Queue
+            LinkedListQueue linkedListQueue = new LinkedListQueue();
             switch (option)
             {
                 case 1:
-                    linkedListStack.Push(70);    //Adding data to stack
+                    linkedListStack.Push(70);    //UC1 -Adding data to stack
                     linkedListStack.Push(30);
                     linkedListStack.Push(56);
                    
@@ -22,7 +25,7 @@
                     linkedListStack.Display();
                     break;
                 case 2:
-                    linkedListStack.Push(70);    //UC 2
+                    linkedListStack.Push(70);    //UC-2
                     linkedListStack.Push(30);
                     linkedListStack.Push(56);
                     Console.WriteLine("----------------------------");
@@ -31,14 +34,18 @@
                     Console.WriteLine("----------------------------");
                     linkedListStack.Pop();
                     break;
+                case 3:
+                    linkedListQueue.Enqueue(56);     //UC-3
+                    linkedListQueue.Enqueue(30);
+                    linkedListQueue.Enqueue(70);
+                    Console.WriteLine("----------------------------");
+                    //Display Queue
+                    linkedListQueue.Display();
+                    break;
                 default:
                     Console.WriteLine("Please choose correct option");
                     break;
-            }
-            
-           
-
-            
+            }         
             Console.ReadLine();
         }
     }
